6fc5ffb78e14c694bdf9136591e8e2cd7c455e0f
krishraj9318 <krishrajsinghrao15@gmail.com>
2026-09-03 10:42:29 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+function start() {
+  let text = "Not everything I code is for marks... some are just to make someone smile 🙂";
+  let i = 0;
 
+  function type() {
+    if (i < text.length) {
+      document.getElementById("msg").innerHTML += text[i];
+      i++;
+      setTimeout(type, 40);
+    }
+  }
+
+  type();
+
+  setInterval(createHeart, 300);
+}
+
+function createHeart() {
+  const heart = document.createElement("div");
+  heart.innerHTML = "❤️";
+  heart.style.position = "absolute";
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.top = "100vh";
+  heart.style.fontSize = "20px";
+  heart.style.animation = "float 4s linear";
+
+  document.body.appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 4000);
+}
